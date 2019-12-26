1c51582f380dc869766a90c1426e57360dc76ce3
isaaccarvalho <isaac.oliv.carvalho@gmail.com>
2019-12-26 10:56:22 -0300

    Movimentacao Canhao
    Movimento de rotacao entre o angulo de alcance definido

---
+++ Assets/Scripts/canhao.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canhao : MonoBehaviour
+{
+
+    public float velocidade = 1.5f; // velocidade do canhao
+    public float alcance = 45; // alcance do giro do canhão, em graus
+    public int balas = 5; // balas do canhão
+
+    public GameObject canoCanhao; // cano do canhao que irá girar
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gira();
+    }
+
+    void gira()
+    {
+        float angulo = Mathf.Sin(Time.time * velocidade) * alcance;
+        canoCanhao.transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
+    }
+
+    void atira()
+    {
+        if(balas>0)
+        {
+            balas--;
+        }
+        
+    }
+}
